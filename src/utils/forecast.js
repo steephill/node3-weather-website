1b8589ead454206a8bad2e6212a7d2ab08885d9b
steephill <java410@yahoo.com>
2019-12-17 12:07:24 -0800

    Added high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + '. It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain')
+            callback(undefined, body.daily.data[0].summary + '. High: ' + body.daily.data[0].temperatureHigh + '/ Low: ' + body.daily.data[0].temperatureLow + '. It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain')
         }
     })
 }
